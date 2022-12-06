2e747743b80a821079f0da7a7f601c85b074433b
Alan Garduño Pineda <alan.garduno@elektra.com.mx>
2022-12-06 16:54:03 -0600

    CustomHook para los ToDo's

---
+++ 05-hook-app/src/hooks/useToDo.ts
@@ -0,0 +1,46 @@
+import {useReducer, useEffect} from 'react';
+import {
+	ActionsInterface,
+	TodoInterface,
+	todoReducer,
+} from '../08-useReducer/todoReducer';
+
+export const useToDo = (initialState: TodoInterface[]) => {
+	const init = () => {
+		return JSON.parse(localStorage.getItem('todos') || '[]');
+	};
+
+	const [state, dispatch] = useReducer(todoReducer, initialState, init);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(state) || JSON.stringify([]));
+	}, [state]);
+
+	const handleNewTodo = (todo: TodoInterface) => {
+		const action: ActionsInterface = {
+			type: '[TODO] add todo',
+			payload: todo,
+		};
+
+		dispatch(action);
+	};
+
+	const handleDeleteTodo = (id: number) => {
+		const action: ActionsInterface = {
+			type: '[TODO] delete todo',
+			payload: id,
+		};
+
+		dispatch(action);
+	};
+
+	const handleToggleTodo = (id: number) => {
+		const action: ActionsInterface = {
+			type: '[TODO] update todo',
+			payload: id,
+		};
+
+		dispatch(action);
+	};
+	return {state, handleNewTodo, handleDeleteTodo, handleToggleTodo};
+};
